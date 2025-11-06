58ceb1a0a8b671e5a6234a03a480981b09bb0931
yerayperejon <yerayperejon@gmail.com>
2025-11-06 23:19:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+      base: '/gemini-chat-app/',
     return {
       server: {
         port: 3000,
